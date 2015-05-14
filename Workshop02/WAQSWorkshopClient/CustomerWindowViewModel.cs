a19022687e3da63d12d17658b668fcf282ae132a
Matthieu MEZIL <matthieu.mezil@live.fr>
2015-05-14 00:15:01 -0700

    W02

---
+++ Workshop02/WAQSWorkshopClient/CustomerWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WAQSWorkshopClient;
+using WAQSWorkshopClient.ClientContext;
+using WAQSWorkshopClient.ClientContext.Interfaces;
+using WAQSWorkshopClient.ClientContext.Interfaces.Errors;
+using WAQS.ClientContext;
+using WAQS.ClientContext.Interfaces;
+using WAQS.ClientContext.Interfaces.Errors;
+using WAQS.ClientContext.Interfaces.Querying;
+using WAQS.ComponentModel;
+using System.Windows.Input;
+
+namespace WAQSWorkshopClient
+{
+    public class CustomerWindowViewModel : ViewModelBase
+    {
+        private INorthwindClientContext _context;
+        private CustomerWindow _customerWindow; // using the window here is not good for an MVVM point of view
+
+        public CustomerWindowViewModel(INorthwindClientContext context, string customerId, CustomerWindow customerWindow): base (context)
+        {
+            _context = context;
+            _customerWindow = customerWindow;
+            LoadCustomer(customerId).ConfigureAwait(true);
+        }
+
+        private Customer _customer;
+        public Customer Customer
+        {
+            get { return _customer; }
+            private set
+            {
+                _customer = value;
+                NotifyPropertyChanged.RaisePropertyChanged(nameof(Customer));
+            }
+        }
+
+        private async Task LoadCustomer(string customerId)
+        {
+            Customer = await _context.Customers.AsAsyncQueryable().FirstOrDefault(c => c.Id == customerId).ExecuteAsync();
+        }
+
+        private RelayCommand _saveCommand;
+        public ICommand SaveCommand
+        {
+            get { return _saveCommand ?? (_saveCommand = new RelayCommand(() => SaveChangesAsync().ConfigureAwait(true))); }
+        }
+
+        private async Task SaveChangesAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+                _customerWindow.Close();
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show(e.Message); // this is not good for an MVVM point of view
+            }
+        }
+
+        private RelayCommand _cancelCommand;
+        public ICommand CancelCommand
+        {
+            get { return _cancelCommand ?? (_cancelCommand = new RelayCommand(() => _customerWindow.Close())); }
+        }
+    }
+}
---
+++ Workshop02/WAQSWorkshopClient/MainWindowViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WAQSWorkshopClient;
+using WAQSWorkshopClient.ClientContext;
+using WAQSWorkshopClient.ClientContext.Interfaces;
+using WAQSWorkshopClient.ClientContext.Interfaces.Errors;
+using WAQS.ClientContext;
+using WAQS.ClientContext.Interfaces;
+using WAQS.ClientContext.Interfaces.Errors;
+using WAQS.ClientContext.Interfaces.Querying;
+using WAQS.ComponentModel;
+using System.Windows.Input;
+
+namespace WAQSWorkshopClient
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private INorthwindClientContext _context;
+        private Func<INorthwindClientContext> _contextFactory;
+
+        public MainWindowViewModel(INorthwindClientContext context, Func<INorthwindClientContext> contextFactory) : base(context)
+        {
+            _context = context;
+            _contextFactory = contextFactory;
+        }
+
+        private IEnumerable<CustomerInfo> _customers;
+        public IEnumerable<CustomerInfo> Customers
+        {
+            get
+            {
+                if (_customers == null)
+                {
+                    LoadCustomersAsync().ConfigureAwait(true);
+                }
+                return _customers;
+            }
+            private set
+            {
+                _customers = value;
+                NotifyPropertyChanged.RaisePropertyChanged(nameof(Customers));
+            }
+        }
+
+        public async Task LoadCustomersAsync()
+        {
+            Customers = await (from c in _context.Customers.AsAsyncQueryable()
+                               let totalSpent = c.Orders.Sum(o => o.OrderDetails.Sum(od => od.Quantity * od.UnitPrice * (1 - od.Discount)))
+                               orderby totalSpent descending
+                               select new CustomerInfo
+                               {
+                                   Id = c.Id,
+                                   Name = c.CompanyName + " " + c.ContactName,
+                                   TotalSpent = (double?)c.Orders.Sum(o => o.OrderDetails.Sum(od => od.Quantity * od.UnitPrice * (1 - od.Discount))) ?? 0
+                               }).ExecuteAsync();
+        }
+
+        private RelayCommand _editCommand;
+        public ICommand EditCommand
+        {
+            get
+            {
+                return _editCommand ?? (_editCommand = new RelayCommand(c =>
+                  {
+                      var customerWindow = new CustomerWindow(); // this is not good for an MVVM point of view
+                      var customerWindowViewModel = new CustomerWindowViewModel(_contextFactory(), ((CustomerInfo)c).Id, customerWindow);
+                      customerWindow.DataContext = customerWindowViewModel;
+                      customerWindow.Show();
+                  }));
+            }
+        }
+
+        public class CustomerInfo
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public double TotalSpent { get; set; }
+        }
+    }
+}
