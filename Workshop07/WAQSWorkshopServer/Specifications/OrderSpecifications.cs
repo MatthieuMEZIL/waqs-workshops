89cf0787648d162e8300a4838245ee8cbf233bf7
MatthieuMEZIL <matthieu.mezil@live.fr>
2015-05-24 22:05:09 -0700

    W07

---
+++ Workshop07/WAQSWorkshopServer/Specifications/OrderSpecifications.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace WAQSWorkshopServer.Specifications
+{
+    public static class OrderSpecifications
+    {
+        public static double GetTotal(this Order order)
+        {
+            return order.OrderDetails.Sum(od => od.GetAmount());
+        }
+
+        public static string GetCustomerCompanyName(this Order order)
+        {
+            return order.Customer.CompanyName;
+        }
+
+        public static string GetCustomerContactName(this Order order)
+        {
+            return order.Customer.ContactName;
+        }
+
+        public static bool GetHasInvoice(this Order order)
+        {
+            return order.Invoice != null;
+        }
+    }
+}
