bd2b67b06e1fc78259cd78416a51b500423839d0
MatthieuMEZIL <matthieu.mezil@live.fr>
2015-05-29 01:40:11 -0700

    Add W09

---
+++ Workshop09/WAQSWorkshopClient/WAQS.Northwind/CustomerInfo.cs
@@ -0,0 +1,27 @@
+namespace WAQSWorkshopClient.DTO
+{
+    [System.Runtime.Serialization.DataContractAttribute(IsReference = true, Namespace = "http://Northwind/DTO")]
+    public partial class CustomerInfo
+    {
+        [System.Runtime.Serialization.DataMemberAttribute]
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        [System.Runtime.Serialization.DataMemberAttribute]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [System.Runtime.Serialization.DataMemberAttribute]
+        public double TotalSpent
+        {
+            get;
+            set;
+        }
+    }
+}
